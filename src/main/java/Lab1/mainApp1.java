b09b0b2d8a70c43702557b1cd9210468e2f67706
Henry-Loi <91612066+Henry-Loi@users.noreply.github.com>
2023-09-13 09:42:25 +0800

    Add 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,6 +4,8 @@ public class mainApp1 {
     public static void main(String[] args){
         int b = 2;
         int n = 11;
+
+        // making changes for my third commit
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program started ...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b, n));
